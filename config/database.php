8d32f559f8c6bd66b3d270db280161d2329e3f4c
Mihail161rus <mihail-orlenko@mail.ru>
2018-04-12 22:45:34 +0300

    ver 0.6

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.2'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'faq'),
-            'username' => env('DB_USERNAME', 'root_faq'),
-            'password' => env('DB_PASSWORD', '12345'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'login' => 'admin',
+            'email' => 'admin@admin.ru',
+            'password' => bcrypt('admin'),
+            'role' => 'admin'
+        ]);
+    }
+}
